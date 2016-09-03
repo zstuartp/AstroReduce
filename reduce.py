91b345a67f5415ee44a711db4897d10082d38543
Zackary Parsons <parsons.zackary@gmail.com>
2016-09-02 19:48:47 -0700

    Fixed indentation
    Signed-off-by: Zackary Parsons <parsons.zackary@gmail.com>

--- reduce.py
+++ reduce.py
@@ -191,23 +191,23 @@ class AstroImage:
 
 def update_progress(progress):
 	""" A simple progress bar, accepts a float between 0 and 1 """
-    barLength = 54 # Modify this to change the length of the progress bar
-    status = ""
-    if isinstance(progress, int):
-        progress = float(progress)
-    if not isinstance(progress, float):
-        progress = 0
-        status = "E\r\n"
-    if progress < 0:
-        progress = 0
-        status = "Halt...\r\n"
-    if progress >= 1:
-        progress = 1
-        status = "Done...\r\n"
-    block = int(round(barLength*progress))
-    text = "\rProgress: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), round(progress*100), status)
-    sys.stdout.write(text)
-    sys.stdout.flush()
+	barLength = 54 # Modify this to change the length of the progress bar
+	status = ""
+	if isinstance(progress, int):
+		progress = float(progress)
+	if not isinstance(progress, float):
+		progress = 0
+		status = "E\r\n"
+	if progress < 0:
+		progress = 0
+		status = "Halt...\r\n"
+	if progress >= 1:
+		progress = 1
+		status = "Done...\r\n"
+	block = int(round(barLength*progress))
+	text = "\rProgress: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), round(progress*100), status)
+	sys.stdout.write(text)
+	sys.stdout.flush()
 
 
 def find_astro_imgs(path):
