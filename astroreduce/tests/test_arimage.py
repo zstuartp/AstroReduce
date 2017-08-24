f5b8b9a600d7a0314e477111108e0ffcd87ec505
Zackary Parsons <parsons.zackary@gmail.com>
2017-08-23 22:21:45 -0400

    Improved find_arimgs_in_dir test
    The test should now be a little more robust to detect problems with
    globbing.

--- astroreduce/tests/test_arimage.py
+++ astroreduce/tests/test_arimage.py
@@ -14,6 +14,7 @@ class TestARImage(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree(self._temp_path)
+        self._temp_path = None
 
     def test_find_arimg_in_dir(self):
         subdir_path = os.path.join(self._temp_path, "subdir")
@@ -21,14 +22,17 @@ class TestARImage(unittest.TestCase):
         imgs = []
 
         for i in range(5):
-            # Create 20 images in total, 10 with ".fits" ext and 10 with ".fts" ext
-            # Half of each extension goes in a sub directory to test recursion option
+            # Create 25 images in total, 10 with ".fits" ext and 15 with ".fts" ext
+            # The base dir gets 5 ".fits" and 5 ".fts"
+            # The sub dir gets 5 ".fits" and 10 ".fts"
+            # The sub dir is for checking recursion
             img_prefix_with_base_path = os.path.join(self._temp_path, "testimg-" + str(i))
             img_prefix_with_sub_path = os.path.join(subdir_path, "testimg-" + str(i))
             imgs.append(arimage.ARImage(img_prefix_with_base_path + ".fits", new_file=True))
             imgs.append(arimage.ARImage(img_prefix_with_base_path + ".fts", new_file=True))
             imgs.append(arimage.ARImage(img_prefix_with_sub_path + ".fits", new_file=True))
             imgs.append(arimage.ARImage(img_prefix_with_sub_path + ".fts", new_file=True))
+            imgs.append(arimage.ARImage(img_prefix_with_sub_path + "-2.fts", new_file=True))
 
         for img in imgs:
             img.saveToDisk()
@@ -38,7 +42,7 @@ class TestARImage(unittest.TestCase):
 
         # Check the number of images found
         self.assertEqual(len(base_dir_imgs), 10)
-        self.assertEqual(len(all_imgs), 20)
+        self.assertEqual(len(all_imgs), 25)
 
         # Assert ARImage type
         for img in base_dir_imgs:
